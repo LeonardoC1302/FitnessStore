b6a746706d5a205ef6bca32a6719cce63fe10c88
Leonardo Céspedes <lecespedes@estudiantec.cr>
2024-03-02 20:30:33 -0600

    Navigation started

---
+++ src/components/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Fragment } from 'react'
+import { Outlet, Link } from 'react-router-dom'
+import Logo from '../../../assets/logo.svg'
+import './navigation.styles.scss'
+
+function Navigation(){
+    return (
+        <Fragment>
+            <div className='navigation'>
+                <Link className='navigation__logo' to={'/'}>
+                    <img src={Logo} alt='logo' className='logo' />
+                </Link>
+                <nav className='navigation__links'>
+                    <Link className='nav-link' to={'/shop'}>
+                        Shop
+                    </Link>
+                </nav>
+            </div>
+            <Outlet />
+        </Fragment>
+    )
+}
+
+export default Navigation
