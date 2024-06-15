71fd1e6c57cea9bb8d5157d610fc3fda0cdec6ad
Leonardo Céspedes <101959981+LeonardoC1302@users.noreply.github.com>
2024-06-15 09:48:31 -0600

    Firebase Categories

---
+++ src/shop-data.js
@@ -0,0 +1,89 @@
+const SHOP_DATA = [
+  {
+    title: 'Supplements',
+    items: [
+      {
+        id: 1,
+        name: 'Protein Powder',
+        imageUrl: 'https://i.ibb.co/bF2q1FJ/proteinpowder.jpg',
+        price: 29.99,
+      },
+      {
+        id: 2,
+        name: 'Creatine',
+        imageUrl: 'https://i.ibb.co/kyBgQVs/creatine.jpg',
+        price: 19.99,
+      }
+    ]
+  },
+  {
+    title: 'Weight',
+    items: [
+      {
+        id: 3,
+        name: 'Fat Burner Pills',
+        imageUrl: 'https://i.ibb.co/XzmqKWh/fatburn.jpg',
+        price: 24.99,
+      },
+      {
+        id: 4,
+        name: 'Weight Loss Shakes',
+        imageUrl: 'https://i.ibb.co/7GLZ2VR/weightlossshakes.jpg',
+        price: 34.99,
+      }
+    ]
+  },
+  {
+    title: 'Wellness',
+    items: [
+      {
+        id: 5,
+        name: 'Multivitamins',
+        imageUrl: 'https://i.ibb.co/0CjLSvc/Multivitamins.jpg',
+        price: 14.99,
+      },
+      {
+        id: 6,
+        name: 'Glutamine',
+        imageUrl: 'https://i.ibb.co/vXHdDxG/glutamine.jpg',
+        price: 16.99,
+      }
+    ]
+  },
+  {
+    title: 'Equipment',
+    items: [
+      {
+        id: 7,
+        name: 'Dumbbell Set',
+        imageUrl: 'https://i.ibb.co/SdzF1x7/dumbellset.jpg',
+        price: 59.99,
+      },
+      {
+        id: 8,
+        name: 'Kettlebell',
+        imageUrl: 'https://i.ibb.co/PWbmTz3/Kettlebell.jpg',
+        price: 34.99,
+      }
+    ]
+  },
+  {
+    title: 'Accessories',
+    items: [
+      {
+        id: 9,
+        name: 'Water Bottle',
+        imageUrl: 'https://i.ibb.co/Cn1kyB4/waterbottle.jpg',
+        price: 9.99,
+      },
+      {
+        id: 10,
+        name: 'Gym Bag',
+        imageUrl: 'https://i.ibb.co/28dvD7L/gymbag.jpg',
+        price: 39.99,
+      }
+    ]
+  }
+];
+
+export default SHOP_DATA;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -9,7 +9,7 @@ import {
     signOut,
     onAuthStateChanged
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -39,6 +39,32 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach(obj => {
+        const docRef = doc(collectionRef, obj.title.toLowerCase());
+        batch.set(docRef, obj);
+    });
+
+    await batch.commit();
+};
+
+export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+        const { title, items } = docSnapshot.data();
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }, {});
+    return categoryMap;
+}
+
 export const createUserDocumentFromAuth = async (userAuth, additionalInfo = {}) => {
     if(!userAuth) return;
     const userDocRef = doc(db, 'users', userAuth.uid);
