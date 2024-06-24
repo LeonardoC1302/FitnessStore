6d7ea8b851bbac54b3f1f3ddda2228b0bbe141d8
Leo C <lecespede@estudiantec.cr>
2024-06-24 09:25:49 -0600

    User reducer files & selector

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
 import { UserProvider } from './contexts/user.context.jsx'
 import { CategoriesProvider } from './contexts/categories.context.jsx'
 import { CartDropdownProvider } from './contexts/cart-dropdown.context.jsx'
+import { store } from './store/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
+    <Provider store={ store }>
+      <BrowserRouter>
         <CategoriesProvider>
           <CartDropdownProvider>
             <App />
           </CartDropdownProvider>
         </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,17 @@
 import { Fragment, useContext } from 'react'
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
 import CartIcon from '../../components/cart-icon/cart-icon.component'
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
-import { UserContext } from '../../contexts/user.context'
+import { selectCurrentUser } from '../../store/user/user.selector'
 import { CartDropdownContext } from '../../contexts/cart-dropdown.context'
 import { signOutUser } from '../../utils/firebase/firebase.utils'
 import Logo from '../../assets/logo.svg'
 import { NavigationContainer, LogoContainer, NavLinks, NavLink } from './navigation.styles';
 
 function Navigation(){
-    const { currentUser } = useContext(UserContext);
+    const currentUser = useSelector(selectCurrentUser);
     const { isHidden } = useContext(CartDropdownContext);
 
     return (
